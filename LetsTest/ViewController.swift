8980496403349b5ba8ad759b368591621881d9c6
Yuri Volegov(iosken) <y.volegov@icloud.com>
2023-04-07 15:10:27 +0500

    Initial commit with git ignore

---
+++ LetsTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LetsTest
+//
+//  Created by Yuri Volegov on 07.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
