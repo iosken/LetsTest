c55a0912d8a320dfcdf7b05c311481e166938193
Yuri Volegov(iosken) <y.volegov@icloud.com>
2023-04-10 08:03:20 +0500

    Model develop

---
+++ LetsTest/Model/SwiftFAQ.swift
@@ -0,0 +1,23 @@
+//
+//  SwiftFAQ.swift
+//  LetsTest
+//
+//  Created by Yuri Volegov on 10.04.2023.
+//
+
+import Foundation
+
+struct Secret {
+    let title: String
+    let scheme: String
+    let matherial: [String]
+    let questions: [Question]
+    
+    
+}
+
+struct Question {
+    var title: String
+    var answers: [String]
+    var best: Int
+}
